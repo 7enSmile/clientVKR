e74b613117b88eceebfd8ce0b3f3b3d1d752f8b5
7enSmile <68625468+7enSmile@users.noreply.github.com>
2021-05-05 14:08:17 +0700

    исправлен баг

--- src/model/abstractstudentsmodel.cpp
+++ src/model/abstractstudentsmodel.cpp
@@ -69,25 +69,26 @@ void AbstractStudentsModel::creatFromFile(QString filename)
 
         return;
     }
+    QRegExp regName("(^/D[^;]{1,});(/D[^;]{1,});(/D{1,});(/S[^;]{1,});(/S[^;]{1,})$");
     QStringList wordList;
     QString word;
     while (!file.atEnd()) {
         QByteArray line = file.readLine();
-        if(line.count()==5){
-            word=QString::fromLocal8Bit(line.split(';').at(0));
-            wordList.append(word);
-            word=QString::fromLocal8Bit(line.split(';').at(1));
-            wordList.append(word);
-            word=QString::fromLocal8Bit(line.split(';').at(2));
-            wordList.append(word);
-            word=QString::fromLocal8Bit(line.split(';').at(3));
-            wordList.append(word);
-            word=QString::fromLocal8Bit(line.split(';').at(4));
-            word.remove(word.size()-2,2);
-            wordList.append(word);
-            generateStudent(wordList);
-            wordList.clear();
-        }
+
+        word=QString::fromLocal8Bit(line.split(';').at(0));
+        wordList.append(word);
+        word=QString::fromLocal8Bit(line.split(';').at(1));
+        wordList.append(word);
+        word=QString::fromLocal8Bit(line.split(';').at(2));
+        wordList.append(word);
+        word=QString::fromLocal8Bit(line.split(';').at(3));
+        wordList.append(word);
+        word=QString::fromLocal8Bit(line.split(';').at(4));
+        word.remove(word.size()-2,2);
+        wordList.append(word);
+        generateStudent(wordList);
+        wordList.clear();
+
 
     }
     loadListGlobal();
