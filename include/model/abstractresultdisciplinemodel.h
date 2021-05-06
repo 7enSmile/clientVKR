b09378eac74c3b78160ab2d12c2feb1dab9a4615
7enSmile <68625468+7enSmile@users.noreply.github.com>
2021-05-06 13:24:44 +0700

    Добавлена модель результата дисциплин

--- include/model/abstractresultdisciplinemodel.h
+++ include/model/abstractresultdisciplinemodel.h
@@ -1,11 +1,27 @@
 #ifndef ABSTRACTRESULTDISCIPLINEMODEL_H
 #define ABSTRACTRESULTDISCIPLINEMODEL_H
+#include <QAbstractTableModel>
+#include "Result_education.h"
 
 
-class AbstractResultDisciplineModel
+class AbstractResultDisciplineModel:public QAbstractTableModel
 {
 public:
-    AbstractResultDisciplineModel();
+    AbstractResultDisciplineModel(ListOfResultEducation);
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
+
+    int columnCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
+
+    ListOfResultEducation& getListReports();
+
+    void saveResultEducation(ResultEducation_ptr);
+
+    ResultEducation_ptr getResultEducation(int);
+private:
+    ListOfResultEducation m_listResultEducation;
 };
 
 #endif // ABSTRACTRESULTDISCIPLINEMODEL_H
--- src/model/abstractresultdisciplinemodel.cpp
+++ src/model/abstractresultdisciplinemodel.cpp
@@ -1,6 +1,60 @@
 #include "model/abstractresultdisciplinemodel.h"
 
-AbstractResultDisciplineModel::AbstractResultDisciplineModel()
+AbstractResultDisciplineModel::AbstractResultDisciplineModel(ListOfResultEducation list)
 {
+    m_listResultEducation=list;
+    m_listResultEducation.sortByKey();
+
+}
+
+int AbstractResultDisciplineModel::rowCount(const QModelIndex &parent) const
+{
+     return !parent.isValid() ? m_listResultEducation.count() : 0;
+}
+
+int AbstractResultDisciplineModel::columnCount(const QModelIndex &parent) const
+{
+    return !parent.isValid() ? 1 : 0;
+
+}
+
+QVariant AbstractResultDisciplineModel::data(const QModelIndex &index, int role) const
+{
+    if (role == Qt::DisplayRole)
+        {
+
+            if (index.column() == 0)
+            {
+               return m_listResultEducation.getByIndex(index.row())->getacquired_skills();
+            }
+
+        }
+
+        return QVariant();
+
+}
+
+ListOfResultEducation &AbstractResultDisciplineModel::getListReports()
+{
+    return m_listResultEducation;
+
+}
+
+void AbstractResultDisciplineModel::saveResultEducation(ResultEducation_ptr result)
+{
+    if(m_listResultEducation.count()!=0){
+
+        m_listResultEducation.insert(m_listResultEducation.getKeyByIndex(m_listResultEducation.count()-1)+1,result);
+    }else{
+
+         m_listResultEducation.insert(1,result);
+    }
+
+
+}
+
+ResultEducation_ptr AbstractResultDisciplineModel::getResultEducation(int index)
+{
+    return m_listResultEducation.getByIndex(index);
 
 }
